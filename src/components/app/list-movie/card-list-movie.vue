56ba1109eb5d6653cc7b0feeac95d138e61d37e8
satriayusuf <satriayusuf21@gmail.com>
2022-08-29 02:23:03 +0700

    [ADD] Child Component List Movie

---
+++ src/components/app/list-movie/card-list-movie.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div
+      v-for="(movie, index) in listMovie"
+      :key="index"
+      class="card !rounded-md bg-gray-800 text-white mt-2"
+    >
+      <img
+        :src="'https://image.tmdb.org/t/p/original/' + movie.backdrop_path"
+        class="h-auto w-full"
+        alt="Shoes"
+      />
+      <div class="card-body">
+        <div class="h-full max-h-96 mb-2">
+          <h2 class="card-title">{{ movie.title }}</h2>
+          <div>
+            <p class="text-sm line-clamp-3 break-words">{{ movie.overview }}</p>
+          </div>
+        </div>
+        <div class="card-actions">
+          <button
+            class="btn btn-block btn-outline btn-warning hover:scale-95 hover:duration-300"
+            @click="sendIdForDetailMovie(movie.id)"
+          >
+            View Detail
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+defineProps({
+  listMovie: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const emit = defineEmits(["movieId"]);
+
+function sendIdForDetailMovie(movieId) {
+  emit("movieId", movieId);
+}
+</script>
+
+<style></style>
---
+++ src/components/app/list-movie/header-list-movie.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="block lg:flex">
+    <div class="lg:flex-initial lg:w-3/6">
+      <div class="text-5xl font-bold text-white">
+        <span class="text-yellow-500"> The First </span>
+        <span class="mt-2">Online</span>
+        <div class="mt-2">List Popular Movie</div>
+        <div class="mt-2">Search Engine</div>
+      </div>
+    </div>
+    <div class="lg:flex-initial lg:w-3/6 text-white text-sm">
+      <div class="mt-6 lg:mt-0 mb-8" v-for="desc in descHeader" :key="desc">
+        <span>
+          {{ desc }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const descHeader = ref([
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia fugiat impedit omnis magnam itaque possimus illo corrupti iusto velincidunt magni iste repellat reprehenderit debitis excepturi, estmolestias nam culpa.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia fugiat impedit omnis magnam itaque possimus illo corrupti iusto velincidunt magni iste repellat reprehenderit debitis excepturi, estmolestias nam culpa.",
+]);
+</script>
