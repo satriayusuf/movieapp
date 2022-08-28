4c57ad757cd3c42f62becce223c432b07acf76fd
satriayusuf <satriayusuf21@gmail.com>
2022-08-29 02:23:20 +0700

    [ADD] Skeleton Loader Card List Movie

---
+++ src/components/skeleton/skeleton-card-list-movie.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-10">
+    <div
+      v-for="index in 4"
+      :key="index"
+      class="!rounded-md bg-gray-600 mt-2 animate-pulse py-44 px-14"
+    ></div>
+  </div>
+</template>
