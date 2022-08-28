05fce8437dc760a5a3e9f656066d6da20e3589ea
satriayusuf <satriayusuf21@gmail.com>
2022-08-29 02:23:30 +0700

    [ADD] Navbar

---
+++ src/components/layouts/navbar-list-movie.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pb-5">
+    <router-link to="/" class="text-yellow-500 font-bold text-2xl"
+      >MovieApp</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
