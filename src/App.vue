db4376ea1ae01e70e1bdeb7a0713118f3a50ff6e
satriayusuf <satriayusuf21@gmail.com>
2022-08-29 02:24:56 +0700

    Delete Component HelloWorld and Setup App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="h-full max-h-max bg-slate-800 px-20 md:px-28 py-12">
+    <navbar class="mb-10" />
+    <router-view />
+  </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+<script setup>
+import navbar from "@/components/layouts/navbar-list-movie.vue";
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
