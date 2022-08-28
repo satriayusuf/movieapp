3e87e5758f756be53d6ad4e77dac5733eb61a4b9
satriayusuf <satriayusuf21@gmail.com>
2022-08-29 02:24:26 +0700

    Init Mainjs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import routes from "@/routes/index";
+import axios from "axios";
+import tailwindCss from "@/style/tailwind.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-createApp(App).mount("#app");
+createApp(App).use(routes, axios, tailwindCss).mount("#app");
