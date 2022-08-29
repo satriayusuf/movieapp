da7b60cdfc1c42a71134837e6ec8e5a2e111e711
satriayusuf <satriayusuf21@gmail.com>
2022-08-29 10:09:20 +0700

    [FIX] Grid Card List Movie

--- src/components/app/list-movie/card-list-movie.vue
+++ src/components/app/list-movie/card-list-movie.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
     <div
       v-for="(movie, index) in listMovie"
       :key="index"
